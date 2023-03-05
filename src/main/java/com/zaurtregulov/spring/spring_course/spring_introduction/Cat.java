ddfe221e7c76edc6f5c328ca8054d4206a886024
User <User@Lenovo>
2023-03-06 00:51:52 +0100

    Initial commit

---
+++ src/main/java/com/zaurtregulov/spring/spring_course/spring_introduction/Cat.java
@@ -0,0 +1,10 @@
+
+package com.zaurtregulov.spring.spring_course.spring_introduction;
+
+
+public class Cat implements Pet{
+    @Override
+    public void say(){
+        System.out.println("Meow-Meow");
+    }
+}
